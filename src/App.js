d22ddee613e538424e01bce75b807709cfcc6d15
serhijko <sp_by@tut.by>
2021-08-01 17:58:58 +0300

    Firebase

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LandingScreen from './screens/Landing';
 import SignInScreen from './screens/SignIn';
 import HomeScreen from './screens/Home';
+import Firebase, { FirebaseContext } from './Firebase';
 
 const RootStack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <RootStack.Navigator>
-        <RootStack.Screen name="Landing" component={LandingScreen} />
-        <RootStack.Screen name="Sign In" component={SignInScreen} />
-        <RootStack.Screen name="Home" component={HomeScreen} />
-      </RootStack.Navigator>
-    </NavigationContainer>
+    <FirebaseContext.Provider value={new Firebase()}>
+      <NavigationContainer>
+        <RootStack.Navigator>
+          <RootStack.Screen name="Landing" component={LandingScreen} />
+          <RootStack.Screen name="Sign In" component={SignInScreen} />
+          <RootStack.Screen name="Home" component={HomeScreen} />
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </FirebaseContext.Provider>
   );
 };
 
